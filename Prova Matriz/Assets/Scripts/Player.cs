4e7d3e6bd255673a1be6f4d7e1480b74eb94f057
Gabriel Pedro <168735938+pkzinhogabriel@users.noreply.github.com>
2024-09-18 14:26:40 -0300

    criei a movimentação do personagem 1

--- Prova Matriz/Assets/Scripts/Player.cs
+++ Prova Matriz/Assets/Scripts/Player.cs
@@ -7,15 +7,33 @@ public class Player : MonoBehaviour
     const int velocidade = 2;
     bool jogador1;
     Color cordojogador;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private Vector2 direcao;
+   
+    
     void Update()
     {
-        
+        if (jogador1)
+        {
+            float moveHorizontal = 0;
+            float moveVertical = 0;
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                moveHorizontal = -1;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {  
+                moveHorizontal = 1;
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                moveVertical = -1;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                moveVertical = 1;
+            }
+
+
+        }
     }
 }
