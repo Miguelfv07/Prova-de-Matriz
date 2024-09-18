0d47ab8b79ab5f4e223c6128991c0eb5bb8e2ff3
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-09-18 10:29:17 -0300

    declaração de variaveis

---
+++ Prova Matriz/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject bloco;
+    public GameObject jogador1;
+    public GameObject jogador2;
+    int linha;
+    int coluna;
+    float espacamento;
+    Bloco[,] blocos;
+    int contador;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
